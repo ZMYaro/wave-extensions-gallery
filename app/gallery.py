e78eeaec689b796494a912f94e5ed357f7a5d9a6
Zachary “Gamer_Z.” Yaro <zmyaro@gmail.com>
2012-08-21 02:45:34 -0400

    Added basic code for request handling and redirects in the gallery

--- app/gallery.py
+++ app/gallery.py
@@ -5,3 +5,57 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'templates/head.html')
+		self.response.out.write(template.render(path, {}))
+		path = os.path.join(os.path.dirname(__file__), 'templates/foot.html')
+		self.response.out.write(template.render(path, {}))
+
+class GadgetsPage(webapp.RequestHandler):
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'templates/head.html')
+		self.response.out.write(template.render(path, {}))
+		path = os.path.join(os.path.dirname(__file__), 'templates/foot.html')
+		self.response.out.write(template.render(path, {}))
+
+class RobotsPage(webapp.RequestHandler):
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'templates/head.html')
+		self.response.out.write(template.render(path, {}))
+		path = os.path.join(os.path.dirname(__file__), 'templates/foot.html')
+		self.response.out.write(template.render(path, {}))
+
+class InfoPage(webapp.RequestHandler):
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'templates/head.html')
+		self.response.out.write(template.render(path, {}))
+		path = os.path.join(os.path.dirname(__file__), 'templates/foot.html')
+		self.response.out.write(template.render(path, {}))
+
+class OtherPage(webapp.RequestHandler):
+	def get(self, page):
+		if page == 'extension':
+			self.redirect('/gallery')
+		else:
+			path = os.path.join(os.path.dirname(__file__), 'templates/head.html')
+			self.response.out.write(template.render(path, {}))
+			path = os.path.join(os.path.dirname(__file__), 'templates/404.html')
+			self.response.out.write(template.render(path, {}))
+			path = os.path.join(os.path.dirname(__file__), 'templates/foot.html')
+			self.response.out.write(template.render(path, {}))
+			self.response.set_status(404);
+
+site = webapp.WSGIApplication([('/gallery', MainPage),
+                               ('/gallery/gadgets', GadgetsPage),
+                               ('/gallery/robots', RobotsPage),
+                               ('/gallery/info/(.*)', InfoPage),
+                               ('/(.*)', OtherPage)],
+                              debug=True)
+
+def main():
+	run_wsgi_app(site)
+
+if __name__ == '__main__':
+	main()
